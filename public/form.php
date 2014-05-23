7b7dd3b3931eadc12aa89b9b741abc205c78b973
Jennifer Bochniak <JenAir@Jennifers-MacBook-Air.local>
2014-05-23 08:26:46 -0500

    Added button and placeholders for username and password

--- public/form.php
+++ public/form.php
@@ -12,17 +12,17 @@
     <form method="POST" action="">
         <p>
             <label for="username">Username</label>
-            <input id="username" name="username" type="text">
+            <input id="username" name="username" type="text" placeholder="Username" required>
         </p>
         <p>
             <label for="password">Password</label>
-            <input id="password" name="password" type="password">
+            <input id="password" name="password" type="password" placeholder="Password">
         </p>
         <p>
             <label for="remember">Remember Me?</lable>
             <input id="remember" name="memory" type="checkbox">
-
-            <input type="submit">
+            <br><br>
+            <input type="submit" name="Log In" value="Log In" <button type="button">Click Me!</button>
         </p>
     </form>
 
